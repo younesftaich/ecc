9d4b4105316c1b379d59df04f148f7e531be3ea3
younesftaich <76970345+younesftaich@users.noreply.github.com>
2022-04-18 20:55:30 +0800

    ok

--- resources/js/Pages/utils.js
+++ resources/js/Pages/utils.js
@@ -1,6 +1,6 @@
 export function gettoken() {
   const token = process.env.MIX_TOKEN
-  return "AVAVAj85C435w1EbPcYTmWIShWhhZvekY7b0sHeG6ieGXoGK7UAVhAf6_ag6kF_Od30h3H-sjIM9Uq1r"
+  return "AbHJn6ZYeQ0obUVySZEWhgwlyx7QSu3d9-iLp6DZrmUBnW-BL0vMtwxX3_Ukp0vKgtI9aq9nXwDGyf-d"
   ///return token.toString();
   }
  
